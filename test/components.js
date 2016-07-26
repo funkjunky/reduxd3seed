58aba4a2938e29da6bd0ee39d1f6991c35a2985c
Jason McCarrell <jason.abz@gmail.com>
2016-07-26 02:49:50 -0400

    updated components test to use enzyme

--- test/components.js
+++ test/components.js
@@ -1,4 +1,5 @@
 import TestUtils from 'react-addons-test-utils'
+import { shallow } from 'enzyme';
 import React from 'react';
 
 import App from '../src/components/app.jsx';
@@ -11,12 +12,8 @@ let expect = require('chai').expect;
 
 describe('App component', () => {
     it('Should contain an important header that the client must always have.', () => {
-        let renderer = TestUtils.createRenderer();
-        renderer.render(<App />);
-        let output = renderer.getRenderOutput();
-
-        let [ h1 ] = output.props.children;
-        expect(h1.props.children).to.equal('Product Factors');
+        let app = shallow(<App />);
+        expect(app.find('h1').text()).to.equal('Product Factors');
     });
 });
 
@@ -26,19 +23,17 @@ describe('CircleGraph component', () => {
         {x: 2, y: 2, text: '4', color: 'red'},
     ];
 
-    let renderer = TestUtils.createRenderer();
-    renderer.render(<CircleGraph circles={circles} />);
-    let output = renderer.getRenderOutput();
+    let circleGraph = shallow(<CircleGraph circles={circles} />);
 
     it('should contain tweenablecircles', () => {
-        expect(output.props.children.length).to.equal(2);
+        expect(circleGraph.find('TweenableCircle').length).to.equal(2);
     });
 
     it('should pass x and y to TweenableCircles', () => {
-        output.props.children.forEach((tweenableCircle) => {
+        circleGraph.find('TweenableCircles').forEach((tweenableCircle) => {
             expect(tweenableCircle.type).to.equal(TweenableCircle);
-            expect(tweenableCircle.props).to.have.property('x');
-            expect(tweenableCircle.props).to.have.property('y');
+            expect(tweenableCircle.props()).to.have.property('x');
+            expect(tweenableCircle.props()).to.have.property('y');
         });
     });
 });
@@ -46,35 +41,28 @@ describe('CircleGraph component', () => {
 describe('DirectedCircle component', () => {
     let circle = {x: 1, y: 1, text: '1', color: 'red'};
 
-    let renderer = TestUtils.createRenderer();
-    renderer.render(<DirectedCircle {...circle} />);
-    let output = renderer.getRenderOutput();
+    let directedCircle = shallow(<DirectedCircle {...circle} />);
 
     it('should contain two lines', () => {
-        expect(output.props.children.filter((element) => element.type === 'line').length).to.equal(2);
+        expect(directedCircle.find('line').length).to.equal(2);
     });
 
     it('should contain a circle with x and y', () => {
-        console.log('fourth: ', output.props.children[3].type, TextedCircle, output.props.children[3].type == TextedCircle);
-        var circles = output.props.children.filter((element) => element.type == TextedCircle);
-        expect(circles.length).to.equal(1);
-        expect(circles[0].props).to.have.property('x', circle.x);
-        expect(circles[0].props).to.have.property('y', circle.y);
+        let textedCircle = directedCircle.find('TextedCircle');
+        expect(textedCircle.length).to.equal(1);
+        expect(textedCircle.props()).to.have.property('x', circle.x);
+        expect(textedCircle.props()).to.have.property('y', circle.y);
     });
 });
 
 describe('TextedCircle component', () => {
-    let circle = {x: 1, y: 1, color: 'red'};
+    let circle = {x: 1, y: 1, text: '1', color: 'red'};
 
-    let renderer = TestUtils.createRenderer();
-    renderer.render(<TextedCircle {...circle} />);
-    let output = renderer.getRenderOutput();
+    var textedCircle = shallow(<TextedCircle {...circle} />);
 
-    let circles = output.props.children.filter((element) => element.type == 'circle');
-    let texts = output.props.children.filter((element) => element.type == 'text');
     it('should contain a circle and text element', () => {
-        expect(circles.length).to.equal(1);
-        expect(texts.length).to.equal(1);
+        expect(textedCircle.find('circle').length).to.equal(1);
+        expect(textedCircle.find('text').length).to.equal(1);
     });
-    it('should contain the correct text in text element', () => expect(texts[0].props.children).to.equal(circle.text));
+    it('should contain the correct text in text element', () => expect(textedCircle.find('text').text()).to.equal(circle.text));
 });
